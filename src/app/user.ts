9e87208852d9a147fd880ebb9aa64009808136f4
Alessandro Chimetto <alessandro.chimetto.it@gmail.com>
2021-05-22 20:16:31 +0200

    Aggiunto header a classi TS preesistenti

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,10 @@
+/*
+ * PORTACS
+ * piattaforma di controllo mobilità autonoma
+ *
+ * Copyright lineCode group <linecode.swe@gmail.com> 2020 - 2021
+ * Distributed under open-source licence (see accompanying file LICENCE).
+ */
 export class User {
   constructor(
     private readonly username: string,
