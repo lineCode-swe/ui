d32f2e466beceae86ab31b624c95c3c1d8d5ca36
Alessandro Chimetto <alessandro.chimetto.it@gmail.com>
2021-05-25 11:17:14 +0200

    HOTFIX: aggiornati metodi dell'interfaccia ServerService

--- src/app/server-service.ts
+++ src/app/server-service.ts
@@ -6,8 +6,25 @@
  * Distributed under open-source licence (see accompanying file LICENCE).
  */
 import {Position} from './position';
+import {AuthStatus} from "./auth-status.enum";
+import {Cell} from "./cell";
+import {PartialObserver} from "rxjs";
+import {User} from "./user";
+import {Unit} from "./unit";
 
 export interface ServerService {
+  getAuthStatus(): AuthStatus;
+  getMapLength(): number;
+  getMapHeight(): number;
+  getCell(position: Position): Cell;
+  getAllCell(): Cell[];
+  subscribeCell(obs: PartialObserver<Position>): void;
+  getUser(username: string): User;
+  getAllUser(): User[];
+  subscribeUser(obs: PartialObserver<string>): void;
+  getUnit(id: string): Unit;
+  getAllUnit(): Unit[];
+  subscribeUnit(obs: PartialObserver<string>): void;
   login(user: string, password: string): void;
   logout(user: string): void;
   addUser(user: string, password: string, admin: boolean): void;
