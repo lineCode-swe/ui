bae5f97c225f04c49163f5cd7a60d41db41f69e4
Alessandro Dindinelli <alessandro.dindinelli@gmail.com>
2021-06-06 11:35:05 +0200

    Update components con Observable

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -20,61 +20,26 @@ import {Unit} from "../unit";
 })
 export class MapComponent {
 
-  private mapLengthTemp: number;
-  private mapHeightTemp: number;
-
   private mapLength: Array<number> = [];
   private mapHeight: Array<number> = [];
 
-  private map: Cell[] = [];
+  private map: Observable<Cell[]>;
 
   constructor(private service: ServerService) {
-    // this.map = this.service.getCellObservable();
 
-    // for (let i: number = 0; i<this.service.getMapLength(); i++) {
-    //   this.mapLength.push(i);
-    // }
-    //
-    // for (let i: number = 0; i<this.service.getMapHeight(); i++) {
-    //   this.mapHeight.push(i);
-    // }
+    this.map = this.service.getCellObservable();
 
-    // TESTING
-
-    this.mapLengthTemp = 4;
-    this.mapHeightTemp = 3;
-    for (let i: number = 0; i<this.mapLengthTemp; i++) {
+    for (let i: number = 0; i<this.service.getMapLength(); i++) {
       this.mapLength.push(i);
     }
 
-    for (let i: number = 0; i<this.mapHeightTemp; i++) {
+    for (let i: number = 0; i<this.service.getMapHeight(); i++) {
       this.mapHeight.push(i);
     }
 
-    let poiCell1: Cell = new Cell(new Position(0, 0), false, true, false, Direction.ALL, false, "A");
-    let poiCell2: Cell = new Cell(new Position(0, 1), false, false, false, Direction.UP, false, "B");
-    let lockCell1: Cell = new Cell(new Position(0, 2), true, false, false, Direction.DOWN, false, "");
-    let baseCell1: Cell = new Cell(new Position(0, 3), false, false, true, Direction.LEFT, false, "");
-
-    let baseCell2: Cell = new Cell(new Position(1, 0), false, false, true, Direction.ALL, false, "C");
-    let lockCell2: Cell = new Cell(new Position(1, 1), true, false, false, Direction.RIGHT, false, "");
-    let poiCell3: Cell = new Cell(new Position(1, 2), false, false, false, Direction.RIGHT, true, "");
-    let poiCell4: Cell = new Cell(new Position(1, 3), false, true, false, Direction.DOWN, false, "");
-
-    let emptyCell1: Cell = new Cell(new Position(1, 0), false, false, false, Direction.ALL, false, "");
-    let emptyCell2: Cell = new Cell(new Position(1, 1), false, false, false, Direction.ALL, false, "");
-    let emptyCell3: Cell = new Cell(new Position(1, 2), false, false, false, Direction.ALL, false, "");
-    let emptyCell4: Cell = new Cell(new Position(1, 3), false, false, false, Direction.ALL, false, "");
-
-    this.map = [
-      poiCell1, poiCell2, lockCell1, baseCell1,
-      baseCell2, lockCell2, poiCell3, poiCell4,
-      emptyCell1, emptyCell2, emptyCell3, emptyCell4
-    ];
-
   }
 
-  getMap(): Cell[] {
+  getMap(): Observable<Cell[]> {
     return this.map;
   }
 
@@ -90,7 +55,7 @@ export class MapComponent {
     let htmlStr: string = '';
     let pos: Position= new Position(i, j);
     if (cell.getPosition().toString() == pos.toString()) {
-      // OBSTACLE + UNITS
+      // OBSTACLE + UNITS (WOULD BE AN ERROR)
       if (cell.getObstacle() && cell.getUnit() != "") {
         htmlStr += '\xa0O<sup>U</sup>\xa0';
       }
@@ -100,7 +65,7 @@ export class MapComponent {
       }
       // UNITS
       else if (cell.getUnit() != "") {
-        htmlStr += '\xa0U\xa0';
+        htmlStr += '\xa0U<sup>' + cell.getUnit() + '</sup>\xa0';
       }
     }
     return htmlStr;
--- src/app/unit-management/unit-management.component.ts
+++ src/app/unit-management/unit-management.component.ts
@@ -8,8 +8,7 @@
 import {Component} from '@angular/core';
 import {ServerService} from "../server-service";
 import {Unit} from "../unit";
-import {Position} from "../position";
-import {UnitStatus} from "../unit-status.enum";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-unit-management',
@@ -18,41 +17,24 @@ import {UnitStatus} from "../unit-status.enum";
 })
 export class UnitManagementComponent {
 
-  private units: Unit[] = [];
-  private unitIndex: number = 0;
+  private units: Observable<Unit[]>;
+  private unitId: string;
 
   constructor(private service: ServerService) {
-
-    // this.units = this.service.getUnitObservable();
-
-    let pos1: Position = new Position(0, 0);
-    let pos2: Position = new Position(1, 0);
-    let pos3: Position = new Position(0, 2);
-    let pos4: Position = new Position(1, 3);
-    let pos5: Position = new Position(0, 3);
-    let pos6: Position = new Position(2, 1);
-
-    let unit1: Unit = new Unit('0', 'A', pos1, pos5, [pos1, pos4], UnitStatus.GOINGTO, 0, 50);
-    let unit2: Unit = new Unit('1', 'B', pos3, pos2, [pos4], UnitStatus.STOP, 0, 50);
-    let unit3: Unit = new Unit('2', 'C', pos1, pos1, [pos3, pos2], UnitStatus.BASE, 0, 50);
-    let unit4: Unit = new Unit('3', 'D', pos4, pos3, [pos1], UnitStatus.STOP, 0, 50);
-    let unit5: Unit = new Unit('4', 'E', pos1, pos4, [pos5, pos4], UnitStatus.GOINGTO, 0, 50);
-    let unit6: Unit = new Unit('5', 'F', pos3, pos6, [pos6, pos3], UnitStatus.GOINGTO, 0, 50);
-
-    this.units = [unit1, unit2, unit3, unit4, unit5, unit6];
-
+    this.units = this.service.getUnitObservable();
+    this.unitId = (this.units)[0].getId();
   }
 
-  getUnits(): Unit[] {
+  getUnits(): Observable<Unit[]> {
     return this.units;
   }
 
-  getUnitIndex(): number {
-    return this.unitIndex;
+  getUnitId(): string {
+    return this.unitId;
   }
 
-  selectUnit(unitIndex: number): void {
-    this.unitIndex = unitIndex;
+  selectUnit(unitId: string): void {
+    this.unitId = unitId;
   }
 
 }
